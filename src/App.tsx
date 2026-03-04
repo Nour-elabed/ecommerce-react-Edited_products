72c569b8c2cedd7109ce44873cb84a291ede139c
Nour1717 <elabed.nour@gmail.com>
2026-03-04 14:14:05 +0100

    Login and Register components

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,25 @@ import Product from './_root/pages/Product'
 import './index.css'
 import RootLayout from './_root/pages/RootLayout'
 import Login from './_root/pages/Login'
+import { useEffect, useState } from 'react'
+import Register from './_root/pages/Register'
 
 const App = () => {
+  const [user, setUser] = useState(null)
+  const [error, setError] = useState("")
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token= localStorage.getItem("token")
+  }, [])
   return (
     <Routes>
       <Route element={<RootLayout />}>
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/product' element={<Product />} />
-        <Route path='/login' element={<Login/>}  />
+        <Route path='/login' element={<Login setUser={setUser}/>}  />
+                <Route path='/register' element={<Register setUser={setUser}/>}  />
+
       </Route>
     </Routes>
   )
---
+++ src/_root/pages/Register.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar"
+const Register = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Register
