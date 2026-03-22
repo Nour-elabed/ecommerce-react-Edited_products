a799cfb475e9476e6a8ecb9c03cdf34c60de9c89
Nour1717 <elabed.nour@gmail.com>
2026-03-22 14:38:20 +0100

    seperated backend

--- src/_root/pages/Orders.tsx
+++ src/_root/pages/Orders.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { getUserOrders } from '@/services/orderService'
